993e4a6da6ecd63fabda0b63606a6d7613d8f53c
Pavan Prabhakar Bhat <pxb8715@rit.edu>
2018-04-27 02:22:38 -0400

    (master) adding README

---
+++ README.md
@@ -0,0 +1,1 @@
+# HR Management APP (MEAN Stack)
--- server.js
+++ server.js
@@ -1,1 +1,5 @@
 var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var morgan = require('morgan');
+var mongoose = require('mongoose');
