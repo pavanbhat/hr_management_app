08cc2b059563f857f5ee3e549de910e63789d8bd
Pavan Prabhakar Bhat <pxb8715@rit.edu>
2018-04-30 10:17:38 -0400

    (origin:master) fixed table

--- client/src/app/components/body/hr-view/hr-view.component.ts
+++ client/src/app/components/body/hr-view/hr-view.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {EmployeeService} from "../../../services/employee.service";
 import {Employee} from "../../../models/employee";
 import {MatPaginator, MatSort, MatTableDataSource, PageEvent} from "@angular/material";
-import {Sort} from '@angular/material';
 
 
 @Component({
@@ -16,8 +15,7 @@ export class HrViewComponent implements OnInit {
   keys: any = [];
   dataSource: MatTableDataSource<Employee>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
-  private sortedData;
+  @ViewChild(MatSort) sort : MatSort;
 
   length = 100;
   pageSize = 10;
@@ -31,7 +29,7 @@ export class HrViewComponent implements OnInit {
   }
 
   constructor(private employeeService: EmployeeService) {
-    this.sortedData = this.employees.slice();
+
   }
 
   ngOnInit() {
@@ -45,47 +43,6 @@ export class HrViewComponent implements OnInit {
     this.dataSource.filter = filterValue;
   }
 
-  sortData(sort: Sort) {
-    const data = this.employees.slice();
-    if (!sort.active || sort.direction == '') {
-      this.sortedData = data;
-      return;
-    }
-
-    this.sortedData = data.sort((a, b) => {
-      let isAsc = sort.direction == 'asc';
-      switch (sort.active) {
-        case 'employeeId':
-          return this.compare(a.employeeId, b.employeeId, isAsc);
-        case 'username':
-          return this.compare(a.username, b.username, isAsc);
-        case 'firstName':
-          return this.compare(a.firstName, b.firstName, isAsc);
-        case 'middleName':
-          return this.compare(a.middleName, b.middleName, isAsc);
-        case 'lastName':
-          return this.compare(a.lastName, b.lastName, isAsc);
-        case 'email':
-          return this.compare(a.email, b.email, isAsc);
-        case 'gender':
-          return this.compare(a.gender, b.gender, isAsc);
-        case 'dateOfBirth':
-          return this.compare(a.dateOfBirth, b.dateOfBirth, isAsc);
-        case 'phoneNumber':
-          return this.compare(a.phoneNumber, b.phoneNumber, isAsc);
-        case 'roleId':
-          return this.compare(a.roleId, b.roleId, isAsc);
-        default:
-          return 0;
-      }
-
-    });
-  }
-
-
-  compare(a, b, isAsc) {
-    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
-  }
 
   getEmployees(): void {
     this.employeeService.getEmployees().subscribe(employees => {
