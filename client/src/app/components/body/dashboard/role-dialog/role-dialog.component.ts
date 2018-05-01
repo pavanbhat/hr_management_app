15932df1a746bba969d0819f07309db6ff2fb5e0
Pavan Prabhakar Bhat <pxb8715@rit.edu>
2018-04-30 21:13:31 -0400

    (origin:master) updated package.json

--- client/src/app/components/body/dashboard/role-dialog/role-dialog.component.ts
+++ client/src/app/components/body/dashboard/role-dialog/role-dialog.component.ts
@@ -16,7 +16,7 @@ export class RoleDialogComponent implements OnInit {
   constructor(public employeeRoleDialog: MatDialogRef<RoleDialogComponent>, private employeeService: EmployeeService) { }
 
   ngOnInit() {
-    // this.getEmployees();
+    this.getEmployees();
   }
 
   onNoClick(): void {
@@ -27,5 +27,19 @@ export class RoleDialogComponent implements OnInit {
 
   }
 
+  getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(employees => {
+      this.employees = employees;
+      if (this.employees.length > 0) {
+        this.keys = Object.keys(this.employees[0]);
+        for (var _i = 0; _i < this.keys.length; _i++) {
+          if (this.keys[_i] === '_id' || this.keys[_i] === 'password' || this.keys[_i] === '__v') {
+            delete this.keys[_i];
+          }
+        }
+      }
+    });
+
+  }
 
 }
